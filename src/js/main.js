32cac550f53c72c9af2de89fbae7870e560ac6de
Andrey <ashkrebela@gmail.com>
2020-08-06 11:14:09 +0300

    Project cleanup completed

--- src/js/main.js
+++ src/js/main.js
@@ -1,96 +1,46 @@
-// import Swiper, {Navigation, Pagination, Mousewheel} from 'swiper';
-//
-// Swiper.use([Navigation, Pagination, Mousewheel]);
-//
-// let swiperFirst;
-// swiperFirst = new Swiper('.primary-screen__swiper-container', {
-//     direction: 'horizontal',
-//     loop: true,
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-// });
-//
-// let swiperSecond;
-// swiperSecond = new Swiper('.about-company__swiper-container', {
-//     direction: 'vertical',
-//     slidesPerView: 1,
-//     spaceBetween: 30,
-//     mousewheel: true,
-//     pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//     },
-// });
-//
-// let swiperThird;
-// swiperThird = new Swiper('.case-study__swiper-container', {
-//     direction: 'horizontal',
-//     slidesPerView: 1,
-//     spaceBetween: 30,
-//     mousewheel: true,
-//     pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//     },
-// });
-//
-//
-// //mobil button
-// let menuToggle = document.getElementById('menu-toggle');
-// let menu = document.querySelector('.main-header__menu');
-// let body = document.querySelector('.body');
-// let menuLink = document.getElementsByClassName('main-header__link');
-//
-// menuToggle.addEventListener('click', () => {
-//     toggleClass(menuToggle, 'menu-toggle--active-menu-toggle');
-//     toggleClass(menu, 'main-header__menu--active-menu-toggle');
-//     toggleClass(body, 'body--stop-scrolling');
-//     toggleClass(menuLink, 'menu-header__link--active-menu-toggle');
-// });
-//
-// Array.from(menuLink).forEach(i => {
-//
-//     i.addEventListener('click', () => {
-//         toggleClass(menuToggle, 'menu-toggle--active-menu-toggle');
-//         toggleClass(menu, 'main-header__menu--active-menu-toggle');
-//         toggleClass(body, 'body--stop-scrolling');
-//         toggleClass(menuLink, 'menu-header__link--active-menu-toggle');
-//     });
-// });
-//
-// function toggleClass(el, className) {
-//     el.classList.toggle(className);
-// }
-//
-// //tabs
-// let tab = function () {
-//     let tabNav = document.querySelectorAll('.extra-menu__link'),
-//         tabContent = document.querySelectorAll('.portfolio__sub-box'),
-//         tabName;
-//
-//     tabNav.forEach(item => {
-//         item.addEventListener('click', selectTabNav)
-//     });
-//
-//     function selectTabNav() {
-//         tabNav.forEach(item => {
-//             item.classList.remove('active-tab');
-//         });
-//         this.classList.add('active-tab');
-//         tabName = this.getAttribute('data-tab-name');
-//         selectTabContent(tabName);
-//     }
-//
-//     function selectTabContent(tabName) {
-//         tabContent.forEach(item => {
-//             item.classList.contains(tabName) ? item.classList.add('active-tab') : item.classList.remove('active-tab');
-//         })
-//     }
-// };
-//
-// tab();
-//
-//
-//
+import Swiper, {Navigation, Pagination, Mousewheel} from 'swiper';
+
+Swiper.use([Navigation, Pagination, Mousewheel]);
+
+let swiperFirst;
+swiperFirst = new Swiper('.reviews__swiper-container', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+//mobil button
+let menuToggle = document.getElementById('menu-toggle');
+let menu = document.querySelector('.main-header__menu');
+let body = document.querySelector('.body');
+let menuLink = document.getElementsByClassName('main-header__link');
+
+function toggleClass(el, className) {
+    el.classList.toggle(className);
+}
+
+menuToggle.addEventListener('click', () => {
+    toggleClass(menuToggle, 'menu-toggle--active-menu-toggle');
+    toggleClass(menu, 'main-header__menu--active-menu-toggle');
+    toggleClass(body, 'body--stop-scrolling');
+    toggleClass(menuLink, 'menu-header__link--active-menu-toggle');
+});
+
+if (window.matchMedia("(max-width: 960px)").matches) {
+    Array.from(menuLink).forEach(i => {
+
+        i.addEventListener('click', () => {
+            toggleClass(menuToggle, 'menu-toggle--active-menu-toggle');
+            toggleClass(menu, 'main-header__menu--active-menu-toggle');
+            toggleClass(body, 'body--stop-scrolling');
+            toggleClass(menuLink, 'menu-header__link--active-menu-toggle');
+        });
+    });
+}
+
+
